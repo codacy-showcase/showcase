80d94056bfa964f5e952cc397de48197b96712c8
Rafael Cortês <mrfyda@gmail.com>
2024-01-24 16:25:48 +0000

    feat: example service

--- exit.py
+++ exit.py
@@ -1,5 +1,16 @@
 import requests
+import pg8000.native as pg
 
-r = requests.post("http://www.github.com", auth=("user", "pass"))
 
-exit(1)
+def main(req):
+    conn = pg.Connection("postgres", password="cpsnow")
+
+    query = "SELECT name FROM users WHERE age=" + req.FormValue("age")
+    conn.run(query)
+
+    r = requests.post(
+        "http://github.com/codacy-security/showcase/pull/2/checks?check_run_id=20810885289",
+        auth=("user", "pass"),
+    )
+
+    exit(0)
